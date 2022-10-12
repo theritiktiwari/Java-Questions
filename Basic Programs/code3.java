c8e308b2cad06efec7875c4373108a893f7e5598
Ritik Tiwari <ritiktiwariwork@gmail.com>
2022-10-13 02:31:16 +0530

    Basic Java Programs

---
+++ Basic Programs/code3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class code3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long binary1, binary2;
+        int i = 0, carry = 0;
+        int[] sum = new int[10];
+        System.out.print("Enter first binary number: ");
+        binary1 = scanner.nextLong();
+        System.out.print("Enter second binary number: ");
+        binary2 = scanner.nextLong();
+        while (binary1 != 0 || binary2 != 0){
+            sum[i++] = (int)((binary1 % 10 + binary2 % 10 + carry) % 2);
+            carry = (int)((binary1 % 10 + binary2 % 10 + carry) / 2);
+            binary1 = binary1 / 10;
+            binary2 = binary2 / 10;
+        }
+        if (carry != 0)
+            sum[i++] = carry;
+        --i;
+        System.out.print("Output: ");
+        while (i >= 0) {
+            System.out.print(sum[i--]);
+        }
+        System.out.print("\n");
+    }
+}
---
+++ Basic Programs/code6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class code6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer : ");
+        int num = sc.nextInt();
+        System.out.print("Enter a double : ");
+        double d = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Enter a string : ");
+        String str = sc.nextLine();
+        
+        System.out.println("Int : " + num);
+        System.out.println("Decimal : " + d);
+        System.out.println("String : " + str);
+    }
+}
